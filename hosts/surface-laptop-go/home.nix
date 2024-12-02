03620c629fe6529ca10e40a6dcbd0ae7524e7ebe
Silvan Schmidt <mail@silvanschmidt.ch>
2024-12-02 14:49:12 +0100

    refactor: create pkgs folder

--- hosts/surface-laptop-go/home.nix
+++ hosts/surface-laptop-go/home.nix
@@ -7,23 +7,23 @@
   programs.home-manager.enable = true;
 
   imports = [
-    ../../modules/home/direnv.nix
-    ../../modules/home/firefox.nix
-    ../../modules/home/fish.nix
-    ../../modules/home/fonts
-    ../../modules/home/gobuster.nix
-    ../../modules/home/helix.nix
-    ../../modules/home/john.nix
-    ../../modules/home/jq.nix
-    ../../modules/home/metasploit.nix
-    ../../modules/home/netcat.nix
-    ../../modules/home/nmap.nix
-    ../../modules/home/openvpn.nix
-    ../../modules/home/nix_language.nix
-    ../../modules/home/seclists.nix
-    ../../modules/home/starship.nix
-    ../../modules/home/wireshark.nix
-    ../../modules/home/zed.nix
-    ../../modules/home/zoxide.nix
+    ../../modules/home/pkgs/direnv.nix
+    ../../modules/home/pkgs/firefox.nix
+    ../../modules/home/pkgs/fish.nix
+    ../../modules/home/pkgs/fonts
+    ../../modules/home/pkgs/gobuster.nix
+    ../../modules/home/pkgs/helix.nix
+    ../../modules/home/pkgs/john.nix
+    ../../modules/home/pkgs/jq.nix
+    ../../modules/home/pkgs/metasploit.nix
+    ../../modules/home/pkgs/netcat.nix
+    ../../modules/home/pkgs/nmap.nix
+    ../../modules/home/pkgs/openvpn.nix
+    ../../modules/home/pkgs/nix_language.nix
+    ../../modules/home/pkgs/seclists.nix
+    ../../modules/home/pkgs/starship.nix
+    ../../modules/home/pkgs/wireshark.nix
+    ../../modules/home/pkgs/zed.nix
+    ../../modules/home/pkgs/zoxide.nix
   ];
 }
