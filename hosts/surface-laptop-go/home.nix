3f6ae9d052e9fdf2ab46c6b6acbd25290a45bd12
Silvan Schmidt <mail@silvanschmidt.ch>
2024-12-02 14:56:54 +0100

    refactor: create collection for cybersec

--- hosts/surface-laptop-go/home.nix
+++ hosts/surface-laptop-go/home.nix
@@ -7,22 +7,17 @@
   programs.home-manager.enable = true;
 
   imports = [
+    ../../modules/home/collections/cybersec.nix
+  
     ../../modules/home/pkgs/direnv.nix
     ../../modules/home/pkgs/firefox.nix
     ../../modules/home/pkgs/fish.nix
     ../../modules/home/pkgs/fonts
-    ../../modules/home/pkgs/gobuster.nix
     ../../modules/home/pkgs/helix.nix
-    ../../modules/home/pkgs/john.nix
     ../../modules/home/pkgs/jq.nix
-    ../../modules/home/pkgs/metasploit.nix
-    ../../modules/home/pkgs/netcat.nix
-    ../../modules/home/pkgs/nmap.nix
     ../../modules/home/pkgs/openvpn.nix
     ../../modules/home/pkgs/nix_language.nix
-    ../../modules/home/pkgs/wordlists.nix
     ../../modules/home/pkgs/starship.nix
-    ../../modules/home/pkgs/wireshark.nix
     ../../modules/home/pkgs/zed.nix
     ../../modules/home/pkgs/zoxide.nix
   ];
---
+++ modules/home/collections/cybersec.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  imports = [
+    ../pkgs/gobuster.nix
+    ../pkgs/john.nix
+    ../pkgs/metasploit.nix
+    ../pkgs/netcat.nix
+    ../pkgs/nmap.nix
+    ../pkgs/wordlists.nix
+    ../pkgs/wireshark.nix
+  ];
+}
