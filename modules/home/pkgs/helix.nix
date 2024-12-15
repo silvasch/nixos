39d411a04d10b73d1f2869ba58a75d842a8d8e87
Silvan Schmidt <mail@silvanschmidt.ch>
2024-12-15 19:16:13 +0100

    feat: configure languages in helix

--- modules/home/pkgs/helix.nix
+++ modules/home/pkgs/helix.nix
@@ -3,12 +3,32 @@
 {
   programs.helix = {
     enable = true;
-
     settings = {
       theme = "adwaita-dark";
       editor = {
         line-number = "relative";
+        true-color = true;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        lsp.display-inlay-hints = true;
+        auto-format = true;
       };
+    };
+    languages = {
+      language-server.rust-analyzer.config.check.command = "clippy";
+
+      language = [
+        {
+          name = "java";
+          indent = {
+            tab-width = 4;
+            unit = "    ";
+          };
+        }
+      ];
     };
   };
 }
